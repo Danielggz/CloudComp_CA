00bad0152d38c100c9cf636bedf90f185a11801b
Danielggz <dggz1993@gmail.com>
2025-11-07 20:05:16 +0000

    Added new files for frontend

---
+++ StoreIt/lib/login.rb
@@ -0,0 +1,13 @@
+class Login
+  #Method for move locations for items
+  def self.moveItem(item, employee)
+  end
+  
+  #Method for pick item
+  def self.pickItem(item)
+  end
+
+  #Method for restock items
+  def self.restockItem(item)
+  end
+end
